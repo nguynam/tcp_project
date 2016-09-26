71916e2237639cf52e57756b3360141d8f9ad800
Nam Nguyen <namnguyen1995@gmail.com>
2016-09-26 09:24:28 -0400

    Added basic error message

--- src/client/client.java
+++ src/client/client.java
@@ -15,30 +15,39 @@ class tcpclient {
 		String address = inFromUser.readLine();
 		System.out.println("Enter server port: ");
 		int port = Integer.parseInt(inFromUser.readLine());
-
-		Socket clientSocket = new Socket(address, port);
-		// creates a socket with address and port and attempts to make
-		// connection.
-		DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
-		BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-		System.out.println("Enter a file name: ");
-		String fileName = inFromUser.readLine();
-		// Capture expected file name
-		outToServer.writeBytes(fileName + '\n');
-		// send request for file
-		FileOutputStream fileIn = new FileOutputStream(fileName);
-		// Create file input stream
-		byte[] buffer = new byte[1024];
-		int count = 0;
-		//Read input stream into buffer, then write current buffer state to file.
-		while (count != -1) {
-			count = clientSocket.getInputStream().read(buffer, 0, 1024);
-			fileIn.write(buffer);
+		if(port > 65535){
+			System.out.println("Invalid port number.");
 		}
-		fileIn.close();
-		inFromServer.close();
-		outToServer.close();
-		clientSocket.close();
+
+		try{
+			Socket clientSocket = new Socket(address, port);
+		
+			// creates a socket with address and port and attempts to make
+			// connection.
+			DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
+			BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+			System.out.println("Enter a file name: ");
+			String fileName = inFromUser.readLine();
+			// Capture expected file name
+			outToServer.writeBytes(fileName + '\n');
+			// send request for file
+			FileOutputStream fileIn = new FileOutputStream(fileName);
+			// Create file input stream
+			byte[] buffer = new byte[1024];
+			int count = 0;
+			//Read input stream into buffer, then write current buffer state to file.
+			while (count != -1) {
+				count = clientSocket.getInputStream().read(buffer, 0, 1024);
+				fileIn.write(buffer);
+			}
+			fileIn.close();
+			inFromServer.close();
+			outToServer.close();
+			clientSocket.close();
+		}
+		catch(Exception e){
+			System.out.println("Could not connect to server.");
+		}
 
 	}
 
