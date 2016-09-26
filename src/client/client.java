2d7881879a95b933c2547d56cce6415cb66ed4e6
Nam Nguyen <namnguyen1995@gmail.com>
2016-09-26 10:39:47 -0400

    Error message for address

--- src/client/client.java
+++ src/client/client.java
@@ -10,6 +10,7 @@ import java.net.Socket;
 class tcpclient {
 
 	public static void main(String args[]) throws Exception {
+		String correctAddress = "127.0.0.1";
 		BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
 		System.out.println("Enter server address: ");
 		String address = inFromUser.readLine();
@@ -17,6 +18,9 @@ class tcpclient {
 		int port = Integer.parseInt(inFromUser.readLine());
 		if(port > 65535){
 			System.out.println("Invalid port number.");
+		}
+		if(!address.equals(correctAddress)){
+			System.out.println("Invalid address.");
 		}
 
 		try{
